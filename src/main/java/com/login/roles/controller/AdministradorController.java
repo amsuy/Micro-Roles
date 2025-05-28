193e1df6773dd6315740ff98550d41c734b30646
amsuy <161232769+amsuy@users.noreply.github.com>
2025-05-27 23:02:56 -0600

    Update AdministradorController.java

--- src/main/java/com/login/roles/controller/AdministradorController.java
+++ src/main/java/com/login/roles/controller/AdministradorController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
